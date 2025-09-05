8f1381e8ddcb23d6d74eca6f2199c920252bf21e
Yee1955 <tyee11291129@gmail.com>
2025-09-05 12:00:51 +1000

    remove method from default endpoints, specify host to 127.0.0.1

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import uvicorn, os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    uvicorn.run("nutrihelp_ai.main:app", host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("nutrihelp_ai.main:app", host="127.0.0.1", port=port)
