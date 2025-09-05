5a62d66efee8a5eb9ec430d0446bc2090091d14e
Yee1955 <tyee11291129@gmail.com>
2025-09-05 14:48:55 +1000

    Force CPU

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import uvicorn, os
 
 if __name__ == "__main__":
+    os.environ["CUDA_VISIBLE_DEVICES"] = ""
     port = int(os.environ.get("PORT", "8000"))
     uvicorn.run("nutrihelp_ai.main:app", host="0.0.0.0", port=port, proxy_headers=True, forwarded_allow_ips="*")
