5c0014f97d31db00d80bd5d8300808d0cac66549
Yee1955 <tyee11291129@gmail.com>
2025-09-05 15:41:08 +1000

    increase timeout_keep_alive to 60 secs

--- run.py
+++ run.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     os.environ["CUDA_VISIBLE_DEVICES"] = ""
     port = int(os.environ.get("PORT", "8000"))
     uvicorn.run(
-    "nutrihelp_ai.main:app",
-    host="0.0.0.0",
-    port=port,
-    proxy_headers=True,
-    forwarded_allow_ips="*",
-    timeout_keep_alive=30
-)
+        "nutrihelp_ai.main:app",
+        host="0.0.0.0",
+        port=port,
+        proxy_headers=True,
+        forwarded_allow_ips="*",
+        timeout_keep_alive=60
+    )
