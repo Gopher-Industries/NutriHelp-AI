b605a5305f2a19abd7957e0fdcb8ef28202c841e
Yee1955 <tyee11291129@gmail.com>
2025-09-05 12:25:07 +1000

    minor update onn port specifying

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import uvicorn, os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("nutrihelp_ai.main:app", host="127.0.0.1", port=port)
+    port = int(os.environ.get("PORT", "8000"))
+    uvicorn.run("nutrihelp_ai.main:app", host="0.0.0.0", port=port, proxy_headers=True, forwarded_allow_ips="*")
