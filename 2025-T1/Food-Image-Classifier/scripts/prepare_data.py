0d983202014637ccb0dd61cca42ca059c85c7490
Liam <liam@Liams-MacBook-Pro-3.local>
2026-05-09 13:09:38 +1000

    AI014 expose scan reliability fields

--- 2025-T1/Food-Image-Classifier/scripts/prepare_data.py
+++ 2025-T1/Food-Image-Classifier/scripts/prepare_data.py
@@ -3,7 +3,7 @@ from torchvision import transforms
 from torch.utils.data import DataLoader
 
 def get_food101_dataloaders(data_dir='data', batch_size=32, image_size=(224, 224)):
-    transform = transforms.Compose([
+    train_transform = transforms.Compose([
         transforms.Resize(image_size),
         transforms.RandomHorizontalFlip(),
         transforms.RandomRotation(20),
@@ -13,8 +13,14 @@ def get_food101_dataloaders(data_dir='data', batch_size=32, image_size=(224, 224
         transforms.Normalize(mean=[0.5]*3, std=[0.5]*3)
     ])
 
-    train_dataset = Food101(root=data_dir, split='train', download=True, transform=transform)
-    test_dataset = Food101(root=data_dir, split='test', download=True, transform=transform)
+    test_transform = transforms.Compose([
+        transforms.Resize(image_size),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5]*3, std=[0.5]*3)
+    ])
+
+    train_dataset = Food101(root=data_dir, split='train', download=True, transform=train_transform)
+    test_dataset = Food101(root=data_dir, split='test', download=True, transform=test_transform)
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
@@ -22,5 +28,5 @@ def get_food101_dataloaders(data_dir='data', batch_size=32, image_size=(224, 224
     return train_loader, test_loader
 
 
-get_food101_dataloaders()
-
+if __name__ == "__main__":
+    get_food101_dataloaders()
